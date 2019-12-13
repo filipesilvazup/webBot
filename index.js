68c35833a3f01ea5a57b71c511a561b91c850b5c
Filipe Silva <filipe.silva@zup.com.br>
2019-12-13 16:16:09 -0300

    fix changes bot

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-bot.login('NjU1MDkzMzcyNTk1MzM5MjY1.XfPFhQ.57lVhN8rAidSHyJKtUMO6Draxrc');
+const config = require('./config.json');
 var currentdate = new Date();
+const MIN_INTERVAL = 30000;
+
+bot.login(config.token);
+bot.on('message', message=>{
+    if(message.content.startsWith('!ping')){
+        message.channel.send('pong!');
+        console.log(message.channel);
+    }
+})
 
 setInterval(function(){
-    if(currentdate.getMinutes() ==30 && currentdate.getHours()== 14)
-    {
-        bot.channels.get('geral').send('Link da Daily https://meet.google.com/gqg-zkhb-qfs');
-    }
+    currentdate = new Date()
+    if((currentdate.getMinutes() == 34 || currentdate.getMinutes() == 38 ) &&  currentdate.getHours()== 15)
+        bot.channels.get('573176435439828994').send('Link da Daily https://meet.google.com/gqg-zkhb-qfs');
+    
 }, MIN_INTERVAL)
